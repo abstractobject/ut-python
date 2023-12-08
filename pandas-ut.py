99656709a8ca104359086e7c9da77892a7495b58
abstract_object <hexadecagon@gmail.com>
2023-12-08 14:17:37 -0600

    renamed file, fine-tuning solver

--- pandas-ut.py
+++ pandas-ut.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import tkinter as tk
 import numpy as np
-import re
 import pyexcel as p
 import sys
 import math
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
-from ortools.linear_solver import pywraplp
-from fractions import Fraction
+from ortools.sat.python import cp_model
 
 #required before we can ask for input file
 root = tk.Tk()
@@ -126,6 +124,116 @@ outputDF = pd.concat(outputList, ignore_index=True)
 outputDF.to_excel(output_directory + "//" + " dfOrder.xlsx", sheet_name="Sheet 1")
 
 ###
+#  PREP DATA AND NEST USING CP-SAT SOLVER
+###
+nestDF = outputDF.copy(deep=True)
+nestDF['LENGTH2'] = nestDF['LENGTH2'].apply(lambda x: x*10000)
+nestDF['LENGTH2'] = nestDF.apply(lambda row:(row['LENGTH2'] + 1250) if row['PARTS PER STICK'] != 1 else row['LENGTH2'], axis=1)
+nestDF = nestDF.loc[nestDF.index.repeat(nestDF['TOTAL QTY'])].reset_index(drop=True)
+
+NestWorksetDataFrame = []
+CutTicketWorksetDataFrame = []
+
+def create_data_model():
+    data = {}
+    # part lengths
+    data['weights'] = dfMatType['LENGTH2'].astype(int).values.tolist()
+    data['items'] = list(range(len(data['weights'])))
+    data['bins'] = data['items']
+    # stick size
+    data['bin_capacity'] = dfMatType.iloc[0, 12] * 10000
+    data['material'] = dfMatType.iloc[0, 1]
+    data['stock-code'] = dfMatType.iloc[0, 0]
+    return data
+
+# angle nesting function
+for group, dfMatType in nestDF.groupby(['STOCK CODE', 'STOCK LENGTH']):
+    data = create_data_model()
+
+    # Create the CP-SAT model.
+    model = cp_model.CpModel()
+
+    # Variables
+    # x[i, j] = 1 if item i is packed in bin j.
+    x = {}
+    for i in data['items']:
+        for j in data['bins']:
+            x[(i, j)] = model.NewIntVar(0, 1, 'x_%i_%i' % (i, j))
+
+    # y[j] = 1 if bin j is used.
+    y = {}
+    for j in data['bins']:
+        y[j] = model.NewIntVar(0, 1, 'y[%i]' % j)
+
+    # Constraints
+    # Each item must be in exactly one bin.
+    for i in data['items']:
+        model.Add(sum(x[i, j] for j in data['bins']) == 1)
+
+    # The amount packed in each bin cannot exceed its capacity.
+    for j in data['bins']:
+        model.Add(
+            sum(x[(i, j)] * data['weights'][i] for i in data['items']) <= y[j] *
+            data['bin_capacity'])
+
+    # Objective: minimize the number of bins used.
+    model.Minimize(sum(y[j] for j in data['bins']))
+
+    solver = cp_model.CpSolver()
+    solver.parameters.max_time_in_seconds = 120.0
+    status = solver.Solve(model)
+
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        num_bins = 0
+        bin_usage = 0
+        for j in data['bins']:
+            if solver.Value(y[j]) == 1:
+                bin_items = []
+                bin_weight = 0
+                for i in data['items']:
+                    if solver.Value(x[i, j]) > 0:
+                        bin_items.append(i)
+                        # stick usage
+                        bin_weight += data['weights'][i]
+                        CutTicketDictionary = {'PART': dfMatType.iloc[i,2], 'QTY': 1, 'STOCK CODE': data['stock-code'], 'MAT. DESCRIPTION': data['material'], 'LENGTH': dfMatType.iloc[i,4], 'ID': dfMatType.iloc[i,7], 'NESTED LENGTH': (data['weights'][i])/10000, 'STICK': j}
+                        # List of parts to dataframe
+                        CutTicketDictionaryDataFrame = pd.DataFrame(data=CutTicketDictionary, index=[0])
+                        # Add the parts to the overall list
+                        CutTicketWorksetDataFrame.append(CutTicketDictionaryDataFrame)
+                if bin_items:
+                    # counting number of sticks pulled
+                    num_bins += 1
+        # make list of parts
+        NestDictionary = {'STOCK CODE': data['stock-code'], 'ORDER': num_bins}
+        # list to dataframe
+        NestDictionaryDataFrame = pd.DataFrame(data=NestDictionary, index=[0])
+        # add parts to overall list
+        NestWorksetDataFrame.append(NestDictionaryDataFrame)
+    else:
+        # there's either a fatal problem, or there's too many "good" solutions
+        print('Nesting problem does not have an optimal or feasible solution.')
+
+#new excel file
+PostNestDataFrame = pd.concat(NestWorksetDataFrame, ignore_index=True)
+PostNestDataFrame.to_excel(output_directory + "//" + " NestOrder.xlsx", sheet_name="Sheet 1")
+
+CutTicketDataFrame = pd.concat(CutTicketWorksetDataFrame, ignore_index=True)
+#combining multiple quantities of the same part on the same stick
+CutTicketDataFrame = CutTicketDataFrame.groupby(['PART', 'STOCK CODE', 'MAT. DESCRIPTION', 'LENGTH', 'NESTED LENGTH', 'ID', 'STICK'])['QTY'].sum(numeric_only=True).reset_index()
+CutTicketDataFrame['SHOP NOTES'] = "CUT " + CutTicketDataFrame['QTY'].apply(str) + " PCS @ " + CutTicketDataFrame['LENGTH']
+CutTicketDataFrame['RAW MAT QTY'] = None
+CutTicketDataFrame['HEAT NUMBER'] = None
+CutTicketDataFrame['LOCATION'] = None
+CutTicketDataFrame['GRADE'] = CutTicketDataFrame['MAT. DESCRIPTION'].str.split("'").str[-1]
+CutTicketDataFrame = CutTicketDataFrame[['PART', 'QTY', 'STOCK CODE', 'GRADE', 'MAT. DESCRIPTION', 'RAW MAT QTY', 'HEAT NUMBER', 'LOCATION', 'SHOP NOTES', 'LENGTH', 'ID', 'NESTED LENGTH', 'STICK']]
+CutTicketDataFrame.to_excel(output_directory + "//" + " DEBUGCutTicket.xlsx", sheet_name="Sheet 1")
+writer = pd.ExcelWriter(output_directory + "//" + " CutTicket.xlsx")
+for group, MaterialGroup in CutTicketDataFrame.groupby(['STOCK CODE']):
+    MaterialGroup = MaterialGroup.sort_values(by=['STICK'])
+    MaterialGroup.to_excel(writer, sheet_name=MaterialGroup.iloc[0,2])
+writer.close()
+
+###
 #  GET ORDER QUANTITY PER MATERIAL ACROSS ALL PROJECTS
 ###
 dfOrder = outputDF.groupby(['STOCK CODE'])[['ROUNDED', 'MINIMUM STOCK']].sum(numeric_only=True).reset_index()
@@ -141,10 +249,10 @@ dfOrder.to_excel(output_directory + "//" + " UT Linear Order.xlsx", sheet_name="
 ###
 #  GET ALLOCATION QUANTITY PER PROJECT
 ###
-writer = pd.ExcelWriter(output_directory + "//" + " UT Linear Allocation.xlsx")
+writerAllocate = pd.ExcelWriter(output_directory + "//" + " UT Linear Allocation.xlsx")
 dfAllocate = outputDF.groupby(['STOCK CODE', 'PROJECT'])['ROUNDED'].sum(numeric_only=True).reset_index()
 for group, StationGroup in dfAllocate.groupby(['PROJECT']):
     StationGroup = StationGroup[['STOCK CODE', 'ROUNDED', 'PROJECT']]
     StationGroup.rename(columns = {'ROUNDED':'ALLOCATE'}, inplace=True)
-    StationGroup.to_excel(writer, sheet_name=StationGroup.iloc[0,2])
-writer.close()
+    StationGroup.to_excel(writerAllocate, sheet_name=StationGroup.iloc[0,2])
+writerAllocate.close()
